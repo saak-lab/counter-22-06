f2ee95659c62d17031c468438c608ea5fd3f529e
saak-lab <saaaak987@gmail.com>
2022-06-23 18:27:33 +0300

    counter” git commit -m counter”

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,24 @@
-const button5 = document.querySelector("#plusFive")
-const indicator = document.querySelector("#number-indicator")
-const disp = document.querySelector("#display-heading")
+const button5 = document.querySelector("#plusFive");
+const button10 = document.querySelector("#plusTen");
+const button15 = document.querySelector("#plusFifteen");
+const button20 = document.querySelector("#plusTwenty");
+const indicator = document.querySelector("#number-indicator");
+const disp = document.querySelector("#display-heading");
 
-button5.addEventListener('click', () => {
-  disp.textContent = "added 5"
-  indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
-})
+button5.addEventListener("click", () => {
+  disp.textContent = "added 5";
+  indicator.textContent = (parseInt(indicator.textContent) + 5).toString();
+});
+
+button10.addEventListener("click", () => {
+  disp.textContent = "added 10";
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString();
+});
+button15.addEventListener("click", () => {
+  disp.textContent = "added 10";
+  indicator.textContent = (parseInt(indicator.textContent) + 15).toString();
+});
+button20.addEventListener("click", () => {
+  disp.textContent = "added 10";
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString();
+});
